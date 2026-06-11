1a61539ead1b3773f779d4aecc839e7947d1d1cb
Gomti <gomtikumari26@gmail.com>
2026-06-12 00:37:37 +0530

    Added achievements and timeline data

---
+++ data/achievements.ts
@@ -0,0 +1,7 @@
+export const achievements = [
+  "IEEE Published Author",
+  "Siemens Scholar (Batch 12)",
+  "Katalyst Scholar",
+  "Hackathon Finalist",
+  "State-Level Yoga Achievement",
+];
---
+++ data/timeline.ts
@@ -0,0 +1,31 @@
+export const timeline = [
+  {
+    year: "2021",
+    title: "Class X",
+    description: "94.8% CBSE",
+  },
+
+  {
+    year: "2023",
+    title: "Class XII",
+    description: "87% CBSE",
+  },
+
+  {
+    year: "2024",
+    title: "Started B.Tech AI",
+    description: "IGDTUW",
+  },
+
+  {
+    year: "2025",
+    title: "Machine Learning Internship",
+    description: "Anveshan Foundation",
+  },
+
+  {
+    year: "2026",
+    title: "IEEE Publication",
+    description: "ICAECT Conference",
+  },
+];
